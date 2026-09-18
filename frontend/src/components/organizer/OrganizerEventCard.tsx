fe3a1bca28a8807dc0bc3f474e76d252ecb64602
Ayoub Ait ihia <148449812+Ayoubaitihia@users.noreply.github.com>
2026-09-18 23:46:26 +0000

    v-19-09-00-46

--- frontend/src/components/organizer/OrganizerEventCard.tsx
+++ frontend/src/components/organizer/OrganizerEventCard.tsx
@@ -6,29 +6,29 @@ import BookingProgressBar from "@/components/organizer/BookingProgressBar"
 
 export default function OrganizerEventCard(){
     return (
-        <div className="border rounded-xl p-3 flex flex-col gap-y-2">
-            <div className="flex items-center">
-                <Badge variant="default">Published</Badge>
-            </div>
-            <div className="flex items-start justify-between">
-                <div>
-                    <h1 className="font-medium text-[15px]">Next.js & Laravel Conference 2026</h1>
-                    <p className="text-[13px] font-light">14 Jun · Sofitel Agadir · 400 capacity</p>
+            <div className="border rounded-xl p-3 flex flex-col gap-y-2">
+                <div className="flex items-center">
+                    <Badge variant="default">Published</Badge>
                 </div>
-            
-                <div className="flex items-center gap-x-1">
-                    <div className="rounded-sm border p-1 cursor-pointer">
-                        <RiGroupLine className="text-sm"/>
+                <div className="flex items-start justify-between">
+                    <div>
+                        <h1 className="font-medium text-[15px]">Next.js & Laravel Conference 2026</h1>
+                        <p className="text-[13px] font-light">14 Jun · Sofitel Agadir · 400 capacity</p>
                     </div>
-                     <div className="rounded-sm border p-1 cursor-pointer">
-                        <FiEdit className="text-sm"/>
-                    </div>
-                    <div className="rounded-sm border p-1 cursor-pointer">
-                        <SlOptions className="text-sm"/>
+                
+                    <div className="flex items-center gap-x-1">
+                        <div className="rounded-sm border p-1 cursor-pointer">
+                            <RiGroupLine className="text-sm"/>
+                        </div>
+                        <div className="rounded-sm border p-1 cursor-pointer">
+                            <FiEdit className="text-sm"/>
+                        </div>
+                        <div className="rounded-sm border p-1 cursor-pointer">
+                            <SlOptions className="text-sm"/>
+                        </div>
                     </div>
                 </div>
+                <BookingProgressBar/>
             </div>
-            <BookingProgressBar/>
-        </div>
     )
 }
